f5bdcda1606c7ce618e9577113e99e94ae7e28e3
JEEVANA-MALLEPADDI <127713904+JEEVANA-MALLEPADDI@users.noreply.github.com>
2024-02-11 19:45:24 +0530

    Arrays

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -26,15 +26,7 @@ class Solution:
             arr.append(r)
 
         return arr
-
-
-
 #(or)
-
-
-
-
-
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         if numRows <= 0:
@@ -47,3 +39,40 @@ class Solution:
                     row[j] = triangle[i - 1][j-1] + triangle[i-1][j]
             triangle.append(row)
         return triangle
+
+
+#another sol---------------------------------------------------------------------------------
+
+from typing import *
+
+def nCr(n, r):
+    res = 1
+
+    # calculating nCr:
+    for i in range(r):
+        res = res * (n - i)
+        res = res // (i + 1)
+    return int(res)
+
+def pascalTriangle(n : int) -> List[List[int]]:
+    ans = []
+
+    #Store the entire pascal's triangle:
+    for row in range(1, n+1):
+        tempLst = [] # temporary list
+        for col in range(1, row+1):
+            tempLst.append(nCr(row - 1, col - 1))
+        ans.append(tempLst)
+    return ans
+
+if __name__ == '__main__':
+    n = 5
+    ans = pascalTriangle(n)
+    for it in ans:
+        for ele in it:
+            print(ele, end=" ")
+        print()
+
+#Time Complexity: O(n*n*r) ~ O(n3), where n = number of rows, and r = column index.
+#Reason: The row loop will run for approximately n times. And generating a row using the naive approach of variation 2 takes O(n*r) time complexity.
+#Space Complexity: In this case, we are only using space to store the answer. That is why space complexity can be still considered as O(1).
